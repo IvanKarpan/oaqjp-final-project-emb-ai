6c7f4f6c5430ebf58cb2f3ccd6dd7de9c98d4cf9
Ivan Karpan <ivan.karpan@onereach.com>
2025-03-12 19:55:46 +0000

    feat: package the application

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=invalid-name
+"""
+Package initialization for the EmotionDetection package.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ["emotion_detector"]
